f4e76a37d875bf270e654a7b41f1e4e76505abdb
Rajarshi Chakraborty <rajarshibz@gmail.com>
2024-08-19 22:37:11 +0530

    Update home openGraph Image

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 
 export const CMS_NAME = "Markdown";
 export const HOME_OG_IMAGE_URL =
-  "https://icpcoins.com/static/media/icpcoins.9be6876ec0844117eda3.png";
+  "/favicon/icon.svg";
 
 
 export const GITHUB_URL = "https://github.com/Neutrinomic";
