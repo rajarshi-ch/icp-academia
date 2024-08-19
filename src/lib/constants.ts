24413d4c1343830ed817031132de14ebe7c5893a
Rajarshi Chakraborty <rajarshibz@gmail.com>
2024-08-19 22:49:05 +0530

    update opengraph Image

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 
 export const CMS_NAME = "Markdown";
 export const HOME_OG_IMAGE_URL =
-  "/favicon/icon.svg";
+  "/assets/opengraph/academy_og_image.jpg";
 
 
 export const GITHUB_URL = "https://github.com/Neutrinomic";
