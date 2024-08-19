8285f929693434126bd5a5d601095b50b6eb1af2
Rajarshi Chakraborty <rajarshibz@gmail.com>
2024-08-19 22:22:11 +0530

    remove unnecessary imports

--- src/app/_components/cards/glossary-card.tsx
+++ src/app/_components/cards/glossary-card.tsx
@@ -1,19 +1,5 @@
 import React from "react";
-import {
-  Box,
-  Heading,
-  Image,
-  Text,
-  Tag,
-  HStack,
-  VStack,
-  Flex,
-  Badge,
-  AspectRatio,
-} from "@chakra-ui/react";
-import { TimeIcon } from "@chakra-ui/icons";
-import DifficultyBadge from "../difficulty-badge";
-import { formatDate } from "@/lib/utils";
+import { Box, Text, VStack, Badge } from "@chakra-ui/react";
 import { GlossaryItem } from "@/interfaces/glossary";
 import LearnMoreButton from "../buttons/learn-more-button";
 import { BodyText } from "../styled-text";
@@ -37,13 +23,20 @@ const GlossaryCard: React.FC<GlossaryCardProps> = ({ glossaryTerm }) => {
       }}
     >
       <VStack align="start" p={4} spacing={5} py={8}>
-        <Badge  bg="secondaryBlue" color='white' fontSize="sm" py={1.5} px={3} borderRadius='md' >
+        <Badge
+          bg="secondaryBlue"
+          color="white"
+          fontSize="sm"
+          py={1.5}
+          px={3}
+          borderRadius="md"
+        >
           Glossary
         </Badge>
         <Text fontSize="lg" fontWeight={600}>
           {glossaryTerm.title}
         </Text>
-        <Box w="20%" h="3px" bg="secondaryBlue" borderRadius='full' />
+        <Box w="20%" h="3px" bg="secondaryBlue" borderRadius="full" />
         <BodyText fontSize="sm">{glossaryTerm.description}</BodyText>
         <LearnMoreButton url={`/glossary/${glossaryTerm.slug}`} />
       </VStack>
